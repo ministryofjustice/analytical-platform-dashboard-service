23b3ae58c2a1b1d9db3f9785a081117d1b574d1c
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2025-03-25 09:58:28 +0000

    Initial project setup
    - Init UV
    - Add django, black
    - Create project structure

---
+++ dashboard_service/settings/local.py
@@ -0,0 +1,19 @@
+from .common import *  # noqa
+
+DEBUG = True
+
+ALLOWED_HOSTS = [".localhost", "127.0.0.1"]
+
+ENV = "local"
+
+# INSTALLED_APPS += [  # noqa
+#     "debug_toolbar",
+# ]
+
+# MIDDLEWARE += [  # noqa
+#     "debug_toolbar.middleware.DebugToolbarMiddleware",
+# ]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
