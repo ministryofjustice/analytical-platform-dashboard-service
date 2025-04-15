1d49edbc2da28db5252e2007b9ad2781bdb98613
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2025-04-15 16:51:31 +0100

    Update entrypoint and related settings

--- dashboard_service/settings/common.py
+++ dashboard_service/settings/common.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import contextlib
 import os
 from pathlib import Path
+from socket import gaierror, gethostbyname, gethostname
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -28,7 +30,12 @@ DEBUG = False
 
 ALLOWED_HOSTS = []
 
-APP_ENV = os.environ.get("APP_ENV")
+try:
+    ALLOWED_HOSTS.append(gethostbyname(gethostname()))
+except gaierror:
+    contextlib.suppress(gaierror)
+
+# Application definition
 
 # Application definition
 INSTALLED_APPS = [
