3b0e0d8be23bfcf306c26b9bee931ffed720a043
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2025-04-25 15:23:34 +0100

    Add missing settings for prod/dev

--- dashboard_service/settings/development.py
+++ dashboard_service/settings/development.py
@@ -3,3 +3,10 @@ from .production import *  # noqa: F401, F403
 DEBUG = True
 
 ALLOWED_HOSTS += ["dashboards.development.analytical-platform.service.justice.gov.uk"]  # noqa: F405
+
+AUTH0_DOMAIN = "dev-analytics-moj.eu.auth0.com"
+AUTH0_AUDIENCE = "urn:control-panel-dev-api"
+
+CONTROL_PANEL_API_URL = (
+    "https://controlpanel.services.dev.analytical-platform.service.justice.gov.uk/api/cpanel/v1/"
+)
