dd5a3f9c2d90aad48457d3b20a287f29e2777b49
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2025-04-25 14:15:53 +0100

    Fix allowed hosts

--- dashboard_service/settings/development.py
+++ dashboard_service/settings/development.py
@@ -2,4 +2,4 @@ from .production import *  # noqa: F401, F403
 
 DEBUG = True
 
-ALLOWED_HOSTS += ["development.dashboards.analytical-platform.service.justice.gov.uk"]  # noqa: F405
+ALLOWED_HOSTS += ["dashboards.development.analytical-platform.service.justice.gov.uk"]  # noqa: F405
