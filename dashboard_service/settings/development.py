02c22c1a032a2da38fd780133d2b7accee27c097
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2025-04-25 16:52:46 +0100

    Add some settings for prod/dev

--- dashboard_service/settings/development.py
+++ dashboard_service/settings/development.py
@@ -4,7 +4,6 @@ DEBUG = True
 
 ALLOWED_HOSTS += ["dashboards.development.analytical-platform.service.justice.gov.uk"]  # noqa: F405
 
-AUTH0_DOMAIN = "dev-analytics-moj.eu.auth0.com"
 AUTH0_AUDIENCE = "urn:control-panel-dev-api"
 
 CONTROL_PANEL_API_URL = (
