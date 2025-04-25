1eefe040cb836756bf891fdec9ae3ec1fa2f9b96
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2025-04-25 15:53:37 +0100

    Release/0.1.2 (#19)
    * Add missing settings for prod/dev
    
    * Bump chart version to 0.1.2

--- dashboard_service/settings/production.py
+++ dashboard_service/settings/production.py
@@ -7,3 +7,10 @@ DEBUG = False
 ALLOWED_HOSTS = ["dashboards.analytical-platform.service.justice.gov.uk"]
 
 ALLOWED_HOSTS.append(gethostbyname(gethostname()))
+
+AUTH0_DOMAIN = "alpha-analytics-moj.eu.auth0.com"
+AUTH0_AUDIENCE = "urn:control-panel-prod-api"
+
+CONTROL_PANEL_API_URL = (
+    "https://controlpanel.services.analytical-platform.service.justice.gov.uk/api/cpanel/v1/"
+)
