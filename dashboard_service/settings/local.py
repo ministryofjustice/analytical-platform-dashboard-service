912b25c16c77cdd3bc4e7fd5c0e351ca4313f3bf
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2025-05-29 14:34:07 +0100

    Remove secret key from local settings (#49)
    * Remove secret key from local settings

--- dashboard_service/settings/local.py
+++ dashboard_service/settings/local.py
@@ -4,8 +4,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = [".localhost", "127.0.0.1"]
 
-SECRET_KEY = "django-insecure-p)0zsf0h@(4$exs814m35ly%x_m8z)z!11n(z^sfl01nsfr+!r"
-
 INSTALLED_APPS += [  # noqa
     "debug_toolbar",
 ]
