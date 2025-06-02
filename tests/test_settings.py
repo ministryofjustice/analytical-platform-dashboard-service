8a7d1a6afd9c4a48181571a7e74136437741dcbe
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2025-06-02 15:17:44 +0100

    Remove redundant test

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -7,9 +7,5 @@ def test_login_required_middleware_enabled():
     assert "django.contrib.auth.middleware.LoginRequiredMiddleware" in MIDDLEWARE
 
 
-def test_session_cookie_age():
-    assert settings.SESSION_COOKIE_AGE == 86400
-
-
 def test_login_prompt_set():
     assert settings.AUTHLIB_OAUTH_CLIENTS["auth0"]["client_kwargs"]["prompt"] == "login"
