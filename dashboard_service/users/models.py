4a0c56b1f8495d7c7700654c1d0dd37b1f362ffa
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2025-07-11 17:13:49 +0100

    Override username, save and add custom UserManager
    There is no need to store the username, as we will use the email
    field instead. So this commit drops the username field and adds a
    custom UserManager to handle user creation.
    Also override the save method to ensure that email is always stored
    lowercase.

--- dashboard_service/users/models.py
+++ dashboard_service/users/models.py
@@ -1,10 +1,52 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+class UserManager(BaseUserManager):
+    """
+    Custom manager for the User model.
+    """
+
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Create and return a user with an email and password.
+        """
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email.lower(), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """
+        Create and return a superuser with an email and password.
+        """
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        return self.create_user(email, password, **extra_fields)
+
+
 class User(AbstractUser):
-    # represents the full user_id stored in auth0
+    # Represents the full user_id stored in auth0
     auth0_id = models.CharField(max_length=255, unique=True, null=True, blank=True)
-    # represents the external provider ID, e.g. the oid in EntraID.
+    # Represents the external provider ID, e.g. the oid in EntraID.
     # This is parsed from the auth0_id
     external_provider_id = models.CharField(max_length=255, null=True, blank=True)
+
+    username = None
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def save(self, *args, **kwargs):
+        """Override save to ensure email is always lowercase."""
+        if self.email:
+            self.email = self.email.lower()
+        super().save(*args, **kwargs)
