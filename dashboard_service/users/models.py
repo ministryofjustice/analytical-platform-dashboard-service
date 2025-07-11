fd9eecb657b95a157d01ea29d7b3155c2116a84a
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2025-07-11 15:34:09 +0100

    Add new fields to User model

--- dashboard_service/users/models.py
+++ dashboard_service/users/models.py
@@ -1,5 +1,10 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    # represents the full user_id stored in auth0
+    auth0_id = models.CharField(max_length=255, unique=True, null=True, blank=True)
+    # represents the external provider ID, e.g. the oid in EntraID.
+    # This is parsed from the auth0_id
+    external_provider_id = models.CharField(max_length=255, null=True, blank=True)
